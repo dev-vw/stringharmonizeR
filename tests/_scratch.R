29bb628cbdc330379d4103c08f97e6a8e28f6352
dev-vw <wang.vania@gmail.com>
2025-08-22 11:14:21 -0700

    add some lines to _scratch.R

--- tests/_scratch.R
+++ tests/_scratch.R
@@ -3,7 +3,8 @@ library(sf)
 
 load("../pepfar-census_poly-match/outputs/objects/shp_lsts.rda")
 
-# burundi -----------------------------------------------------------------
+
+# dr ----------------------------------------------------------------------
 
 cvec <- census_shp_lst$`dominican republic`$adm1 %>% pull(AREA_NAME)
 pvec <- cou_shp_lst$`dominican republic`$adm2 %>% pull(name)
@@ -12,3 +13,12 @@ ovec <- run_harmonizeR(pvec, cvec)
 
 pjoined <- left_join(cou_shp_lst$`dominican republic`$adm2, ovec$vec_tbl, join_by(name == polyname))
 cjoined <- left_join(census_shp_lst$`dominican republic`$adm1, ovec$compvec_tbl, join_by(ADM1_NAME == polyname))
+
+# angola ------------------------------------------------------------------
+
+cvec <- census_shp_lst$angola$adm1 %>% pull(AREA_NAME)
+pvec <- cou_shp_lst$angola$adm1 %>% pull(name)
+
+ovec <- run_harmonizeR(pvec, cvec)
+
+
