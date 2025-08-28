57aa8b515026c775bd9220ec3a66b5e901072b62
dev-vw <wang.vania@gmail.com>
2025-08-28 10:48:29 -0700

    remove tutorial script

--- R/update_rharmonizeR.R
+++
@@ -1,9 +0,0 @@
-# run these two lines if you don't have remotes or devtools installed
-libs <- c("remotes, devtools")
-install.packages(libs)
-
-# fast way to load multiple libraries in one line of code
-lapply(libs, library, character.only = TRUE)
-
-# begin update process
-remotes::update_packages()
